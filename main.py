4c58be9d9726d1769caa481e1d45dd27c4ec9198
Evgen Kofanov <evgeniy.kofanov@gmail.com>
2023-08-26 21:57:32 +0300

    master_0_10_refactor: add function of getting dataframe with lows of trading

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ async def get_order_book(api_key, symbol, limit, futures=False, start_time=None,
     :param limit: (int) number of orders (max 5000);
     :param start_time: (long) time in ms;
     :param end_time: (long) time in ms;
-    :return: (float, float) sum quantities of bid and ask (for number of orders);
+    :return df_asks, df_bids: (Data Frame, Data Frame) orderbooks with asks and bids;
     """
     
     if futures:
@@ -83,11 +83,13 @@ async def get_order_book(api_key, symbol, limit, futures=False, start_time=None,
         async with session.get(url, headers=headers, params=params) as response:
             depth = await response.json()
 
-    df_bids = pd.DataFrame(depth['bids'], columns=['high_price', 'quantity_bid'])
+    df_bids = pd.DataFrame(depth['bids'], columns=['low_price', 'quantity_bid'])
     df_bids = df_bids.astype(float)
     anomal_bids = df_bids[df_bids['quantity_bid'] > anomal_quantity]
     #print(anomal_bids)
     quantity_bid = df_bids['quantity_bid'].sum()
+    print('\\\\\\\\\\\\\\\\\\\\\\\\\\\\')
+    print(anomal_bids)
     
     df_asks = pd.DataFrame(depth['asks'], columns=['high_price', 'quantity_ask'])
     df_asks = df_asks.astype(float)
@@ -98,7 +100,7 @@ async def get_order_book(api_key, symbol, limit, futures=False, start_time=None,
     print(anomal_asks)
 
 
-    return df_asks
+    return df_asks, df_bids
 
 
 
@@ -147,6 +149,49 @@ async def create_df_high(df):
 
 
 
+async def create_df_low(df):
+    """
+    :param df: (pandas Data Frame) CandleStick Data;
+    :return df_high: (pandas Data Frame) CandleStick Highs Data;
+
+    """
+
+    global size_low
+    last_string = 0
+    df_low = pd.DataFrame(columns=['open_time', 'low_price', 'num_kicks'])
+    num = 0
+
+    while num < df.shape[0]:        
+        not_low = False        
+        if df_low.shape[0] == 0:
+            current_line = pd.Series({'open_time': df.iloc[num]['open_time'], 'low_price' : df.iloc[num]['low_price'], 'num_kicks' : 1})
+            df_low.loc[len(df_low.index)] = current_line
+
+        else:
+            last_row_index = df_low.shape[0] - 1           
+            if df_low.iloc[last_row_index]['low_price'] > df.iloc[num]['low_price']:
+                df_low = df_low.drop(last_row_index)
+                num -= 1
+                
+            elif df_low.iloc[last_row_index]['low_price'] == df.iloc[num]['low_price']:
+                num_kick = df_low.loc[last_row_index, 'num_kicks'] + 1
+                df_low.loc[last_row_index, 'num_kicks'] = num_kick
+                print(df_low.loc[last_row_index, 'low_price'], df_low.loc[last_row_index, 'num_kicks'])
+                
+            else:
+                if num >= size_low:
+                    for i in range(size_low):
+                        if float(df.iloc[num]['low_price']) > float(df.iloc[num - i]['low_price']):
+                            not_low = True
+                            break
+                    if not not_low:
+                        current_line = pd.Series({'open_time': df.iloc[num]['open_time'], 'low_price' : df.iloc[num]['low_price'], 'num_kicks' : 1})
+                        df_low.loc[len(df_low.index)] = current_line
+        num += 1
+    
+    df_low['low_price'] = df_low['low_price'].astype(float)
+    return df_low
+
 
                 
     
@@ -159,14 +204,14 @@ symbol = "BTCUSDT"
 interval = '15m'
 
 data = asyncio.run(get_kline_data(api_key, symbol, interval, 500, True))
-data_2 = asyncio.run(get_order_book(api_key, symbol, 1000, True))
-print(type(data_2))
-data_3 = asyncio.run(create_df_high(data))
+data_asks, data_bids = asyncio.run(get_order_book(api_key, symbol, 1000, True))
+print(type(data_bids))
+data_3 = asyncio.run(create_df_low(data))
 print(type(data_3))
 
-merged_df = pd.merge(data_3, data_2, on='high_price')
+merged_df = pd.merge(data_3, data_bids, on='low_price')
 print('\\\\\\\\\\\\\\\\\\\\\\\\\\')
-print(data_2)
+print(data_bids)
 print('\\\\\\\\\\\\\\\\\\\\\\\\\\')
 print(data_3)
 print('\\\\\\\\\\\\\\\\\\\\\\\\\\')
